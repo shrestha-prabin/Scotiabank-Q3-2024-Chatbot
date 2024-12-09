e589b411ca9780c9a1c2cafc1512f52da8e2b9ab
mail.prabin.shrestha@gmail.com <mail.prabin.shrestha@gmail.com>
2024-12-09 14:07:43 -0500

    auto download file

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,17 @@ from flask import Flask, render_template, request, jsonify
 import time
 from app.chatbot import ChatbotModel
 
+import os
+import requests
+
+model_path = 'app/models/bert.pth'
+
+if not os.path.exists(model_path):
+    with requests.get('https://file.io/HyWEg0b2LMaI', stream=True) as r:
+        r.raise_for_status()
+        with open(model_path, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
 
 def create_app(test_config=None):
     # create and configure the app
